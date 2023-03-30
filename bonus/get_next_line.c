db29ac13266f62c01724e2b3d8bfbb8cebef9a21
HASSAN OIGAG <57603770+99Hassan20@users.noreply.github.com>
2023-03-30 12:32:17 +0000

    one more leak

--- bonus/get_next_line.c
+++ bonus/get_next_line.c
@@ -75,7 +75,7 @@ static char	*get_new_stash(char *stash)
 	i = 0;
 	while (stash[i] && stash[i] != '\n')
 		i++;
-	if (!stash[i])
+	if (!stash[i] || !stash[i + 1])
 	{
 		free(stash);
 		return (0);
